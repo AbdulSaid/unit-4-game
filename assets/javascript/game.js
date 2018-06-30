d481022fa770f28af7842150ab45d404378fef0d
Abdul Said <abdulrahman.asaid@gmail.com>
2018-06-30 17:03:30 -0400

    fix game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  // create initial variables
-  var wins = 0;
-  var losses = 0;
-  var targetNumber = computerPick;
-  var counter = 0;
-
   // generate a random number to pop up in the beginning
   var computerPick = Math.floor(Math.random() * 100) + 1;
   $('#randomNumber').text(computerPick);
+
+  // create initial variables
+  var wins = 0;
+  var losses = 0;
+  var counter = 0;
 
   // generate random numbers for the jewels
   var redJewel = Math.floor(Math.random() * 10) + 1;
@@ -28,81 +27,64 @@ $(document).ready(function() {
   document.getElementById('silverJewel').style.cssText = 'display: none';
   document.getElementById('goldJewel').style.cssText = 'display: none';
 
-  // on click events for the jewels
-  $('#buttonRedJewel').on('click', function() {
-    var redCrystalValue = redJewel;
-    counter += redCrystalValue;
-
+  var checkWin = function(value) {
+    counter += value;
     if (counter === computerPick) {
       alert('You win!');
       wins++;
+      $('#winsUpdated2').text('You WIN!');
+      $('#wins2').text('Wins: ' + wins);
+      $('#lossesUpdated2').text('');
+      reset();
     } else if (counter >= computerPick) {
       alert('You lose!!');
       losses++;
+      $('#lossesUpdated2').text('You LOSE!');
+      $('#losses2').text('Losses: ' + losses);
+      $('#winsUpdated2').text('');
+      reset();
     }
     // display new score into html
     $('#updatedScoreId').text('New Score: ' + counter + '!');
-    $('#winsUpdated').text('You WIN!');
-    $('#wins').text('Wins: ' + wins);
-    $('#lossesUpdated').text('You LOSE!');
-    $('#losses').text('Losses: ' + losses);
+  };
+
+  // on click events for the jewels
+  $('#buttonRedJewel').on('click', function() {
+    var redCrystalValue = redJewel;
+    checkWin(redCrystalValue);
   });
 
   $('#buttonBlueJewel').on('click', function() {
     var blueCrystalValue = blueJewel;
-    counter += blueCrystalValue;
-
-    if (counter === computerPick) {
-      alert('You win!');
-      wins++;
-    } else if (counter >= computerPick) {
-      alert('You lose!!');
-      losses++;
-    }
-    // display new score into html
-    $('#updatedScoreId').text('New Score: ' + counter + '!');
-    $('#winsUpdated').text('You WIN!');
-    $('#wins').text('Wins: ' + wins);
-    $('#lossesUpdated').text('You LOSE!');
-    $('#losses').text('Losses: ' + losses);
+    checkWin(blueCrystalValue);
   });
 
   $('#buttonSilverJewel').on('click', function() {
     var silverCrystalValue = silverJewel;
-    counter += silverCrystalValue;
-
-    if (counter === computerPick) {
-      alert('You win!');
-      wins++;
-    } else if (counter >= computerPick) {
-      alert('You lose!!');
-      losses++;
-    }
-    // display new score into html
-    $('#updatedScoreId').text('New Score: ' + counter + '!');
-    $('#winsUpdated').text('You WIN!');
-    $('#wins').text('Wins: ' + wins);
-    $('#lossesUpdated').text('You LOSE!');
-    $('#losses').text('Losses: ' + losses);
+    checkWin(silverCrystalValue);
   });
 
   $('#buttonGoldJewel').on('click', function() {
     var goldCrystalValue = goldJewel;
-    counter += goldCrystalValue;
-
-    if (counter === computerPick) {
-      alert('You win!');
-      wins++;
-    } else if (counter >= computerPick) {
-      alert('You lose!!');
-      losses++;
-    }
-
-    // display new score into html
-    $('#updatedScoreId').text('New Score: ' + counter + '!');
-    $('#winsUpdated').text('You WIN!');
-    $('#wins').text('Wins: ' + wins);
-    $('#lossesUpdated').text('You LOSE!');
-    $('#losses').text('Losses: ' + losses);
+    checkWin(goldCrystalValue);
   });
+
+  var reset = function() {
+    redJewel = Math.floor(Math.random() * 10) + 1;
+    $('#redJewel').text(redJewel);
+
+    blueJewel = Math.floor(Math.random() * 10) + 1;
+    $('#blueJewel').text(blueJewel);
+
+    silverJewel = Math.floor(Math.random() * 10) + 1;
+    $('#silverJewel').text(silverJewel);
+
+    goldJewel = Math.floor(Math.random() * 10) + 1;
+    $('#goldJewel').text(goldJewel);
+
+    computerPick = Math.floor(Math.random() * 100) + 1;
+    $('#randomNumber').text(computerPick);
+
+    counter = 0;
+  };
 });
